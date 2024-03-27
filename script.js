556f7098483bbcac59b30d988d2d36e4437fc90c
Ananya Juneja <junejaananya1234@gmail.com>
2024-03-27 11:10:34 +0530

    done with changes

--- script.js
+++ script.js
@@ -1,124 +1,118 @@
-let searchText='13';
-
-function searchHandler(isShowAll){
-    loading(true);
-    const searchField=document.getElementById("searchField");
-    searchText=searchField.value;
-    loadPhone(searchText,isShowAll);
-    
-}
-const loading= (isLoading)=>
-{
-    
-    const loading= document.getElementById("loading");
-    if(isLoading)
-    {
-        loading.classList.remove('hidden');
-    }
-    else{
-        loading.classList.add('hidden');
-    }
-
-}
-// function searchHandler2(){
-//     const searchField=document.getElementById("searchField2");
-//     searchText=searchField.value;
-//     loadPhone(searchText);
-// }
-const loadPhone= async(searchText,isShowAll)=>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    const data =await res.json();
+const showcase = document.getElementById("showcase");
+async function loadData(id){
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${id}`);
+    const data = await res.json();
     const phones = data.data;
-    displayPhones(phones,isShowAll);
-    
+    if(phones.length == 0){
+        alert("Invalid Input!");
+        return;
+    }
+    populateData(phones);
 }
-loadPhone(searchText);
-const displayPhones = (phones,isShowAll)=>{
-    //console.log(phones);
-    const phoneContainer= document.getElementById("phone-container");
-    phoneContainer.textContent='';
-    
-    const showAll= document.getElementById("showALLBtn");
-    if(phones.length>12 && !isShowAll)
-    {
-        showAll.classList.remove('hidden');
-        
-        
-    }
-    else
-    {
-        showAll.classList.add('hidden');
-    }
-    //display first 10
-    if(!isShowAll)
-    {
-        phones=phones.slice(0,12);
-    }
-    
-    phones.forEach(phone => {
-        //console.log(phone);
-        //1 create a div
-        const phoneCard=document.createElement('div');
-        phoneCard.classList=`card bg-base-100 shadow-xl p-5`;
-        phoneCard.innerHTML=`
-        <figure class="px-10 pt-10">
-                      <img src="${phone.image}" alt="phone" class="rounded-xl" />
-                    </figure>
-                    <div class="card-body items-center text-center">
-                      <h2 class="card-title">${phone.phone_name}</h2>
-                      <p>There are many variations of passages of available, but the majority have suffered</p>
-                      <div class="card-actions">
-                        <button onclick="showDetailsHandler('${phone.slug}')" class="btn btn-primary text-white">Show Details</button>
-                      </div>
-                    </div>
+loadData(13);
+function populateData(phoneData){
+    const showcaseContainer = document.querySelector(".phoneContainer");
+    showcaseContainer.innerHTML = '';
+    phoneData.forEach(eachPhone => {
+        const parentDiv = document.createElement("div");
+        parentDiv.className = "w-[20%] h-[90%] m-2 p-2 rounded-2xl flex flex-col items-center shadow-lg hover:shadow-xl hover:cursor-pointer  mb-10";
 
-        `;
-        phoneContainer.appendChild(phoneCard);
+        const imgDiv = document.createElement("div");
+        imgDiv.className = "h-24 w-24 mb-14";
         
+        const img = document.createElement("img");
+        img.src = eachPhone.image;
+        imgDiv.appendChild(img);
+
+        parentDiv.appendChild(imgDiv);
+
+        const name = document.createElement("h1");
+        name.className = "font-xl font-semibold mb-4";
+        name.id = "phone_name";
+        name.textContent = eachPhone.phone_name;
+
+        parentDiv.appendChild(name);
+
+        const button = document.createElement("button");
+        button.type = 'button';
+        button.className = "m-2 p-2  rounded-full border border-solid border-gray-500";
+        button.textContent = "Show Details";
+        button.addEventListener('click', function() {
+            modalDetail(eachPhone.slug,eachPhone.phone_name);
+        });
+        parentDiv.appendChild(button);
+
+        showcaseContainer.appendChild(parentDiv);
     });
-    
-    loading(false);
-   
+    console.log(phoneData);
 }
-
-// show All Button
-function showBtn()
-{
-   searchHandler(true);
-}
-// Show Details
-const showDetailsHandler = async (id)=>{
-    //console.log(id);
-    // load data
-    const res= await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
-    const data=await res.json();
+function findPhone(){
+    const searchVal = document.getElementById('inputVal').value;
+    loadData(searchVal);
     
-    const phone=data.data;
-    showPhoneDetails(phone);
-    //console.log(phone);
+    const showcaseElement = document.getElementById('showcase');
+    showcaseElement.scrollIntoView({ behavior: 'smooth' });
 }
-const showPhoneDetails=(details)=>{
-    my_modal.showModal();
-    const modelName= document.getElementById('detailsPhoneName');
-    const brandName= document.getElementById('detailsBrand');
-    const detailsSpec= document.getElementById('detailsSpec');
-    const releaseDate= document.getElementById('releaseDate');
-    const imageDiv= document.getElementById('imgContainer');
-
-    imageDiv.innerHTML=`<img src="${details.image}" alt="">`;
-    modelName.innerText=details.name;
-    brandName.innerText=`Brand: ${details.brand}`;
-    const features=details.mainFeatures;
-    
-    console.log(details.image);
-    let string="";
-    for (const key in features) {
-
-      
- string=string+`${key}: ${features[key]} \n`;
-
+async function modalDetail(phoneSlug,phone_name){
+    if (phoneSlug) {
+        const res = await fetch(`https://openapi.programming-hero.com/api/phone/${phoneSlug}`);
+        const data = await res.json();
+        const moreDetails = data.data;
+        console.log(moreDetails);
+        processGetDetails(moreDetails,phone_name);
     }
-    detailsSpec.innerText=string;
-    releaseDate.innerText=`${details.releaseDate}`;
+}
+function processGetDetails(data,name){
+    const my_modal = document.getElementById('my_modal');
+    my_modal.showModal();
+    const form = document.querySelector(".getDetail");
+    form.innerHTML = '';
+    const img = document.createElement("img");
+    img.src = data.image;
+    form.appendChild(img);
+    const ul = document.createElement("ul");
+    ul.className = "w-48 text-sm font-medium text-gray-900 bg-white dark:text-gray-600 w-[90%]";
+    const brand = document.createElement("li");
+    brand.className = "w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600";
+    brand.textContent = "Brand: " + data.brand;
+
+    const li = document.createElement("li");
+    li.className ="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600";
+    li.textContent = "Phone: "+name;
+    const li2 = document.createElement("li");
+    li2.className = "w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600";
+    li2.textContent = "Storage: "+data.mainFeatures.storage;
+
+    const li3 = document.createElement("li");
+    li3.className = "w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600";
+    li3.textContent = "Display: "+data.mainFeatures.displaySize;
+
+    const li4 = document.createElement("li");
+    li4.className = "w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600";
+    li4.textContent = "Chipset: "+data.mainFeatures.chipSet;
+
+    const releaseDate = document.createElement("li");
+    releaseDate.className = "w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600";
+    releaseDate.textContent = "Release Date: "+data.releaseDate;
     
+    ul.appendChild(brand);
+    ul.appendChild(li);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+    ul.appendChild(li4);
+    ul.appendChild(releaseDate);
+    
+    form.appendChild(ul);
+
+    const closeDiv = document.createElement("div");
+    closeDiv.className = "modal-action flex justify-center";
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.className = "btn bg-gray-600 text-white hover:bg-gray-500 hover:shadow-lg rounded-lg m-2 p-2";
+    btn.textContent = "Close";
+    btn.addEventListener('click', function() {
+        my_modal.close(); 
+    });
+    closeDiv.appendChild(btn);
+    form.appendChild(closeDiv);
 }
